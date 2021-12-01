a6af3b4744a6b36aee1218f861840397f29742af
Juan Marron <juan@98point6.com>
2021-11-30 21:16:58 -0800

    mmmmmmmmmmm

--- day1/solve.ts
+++ day1/solve.ts
@@ -1,4 +1,30 @@
 
+
+function solve1(numbers : Array<number>) : number {
+    let count = 0;
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i] > numbers[i-1]) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+function solve2(numbers : Array<number>) : number {
+    let count = 0;
+    let priorSum
+    for (let i = 0; i < numbers.length-2; i++) {
+        let sum = numbers[i] + numbers[i+1] + numbers[i+2]
+        if (sum > priorSum) {
+            count++
+        }
+        priorSum = sum
+    }
+
+    return count;
+}
+
 function readFile() : Array<number> {
     let fs = require('fs');
     let array = fs.readFileSync('input').toString().split("\n").map(x=>+x);
@@ -7,7 +33,8 @@ function readFile() : Array<number> {
 
 function main() {
     let nums = readFile();
-    console.log(`First puzzle: ${nums}`);
+    console.log(`First: ${solve1(nums)}`);
+    console.log(`Second: ${solve2(nums)}`);
 }
 
 main();
